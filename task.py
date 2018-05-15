95420e1e5da0ba13ffe5a865635b447b85ede502
Yurii Vyzhha <yurii.vyzhha@makingwaves.com>
2018-05-15 12:14:51 +0200

    added grenade_assault; removed repeating code

--- task.py
+++ task.py
@@ -1,18 +1,24 @@
 from pyhop import *
 
-move_ap = {'shoot': 5}
+act_ap = {'shoot': 5, 'throw_grenade': 5}
+dmg = {'shoot': 5, 'throw_grenade': 10}
+assault_weapon = {'shoot': 'rifle', 'throw_grenade': 'grenade'}
 
 
 def shoot(state, a, t):
-    if 'rifle' in state.weapons[a] and state.ap[a] >= 5:
-        state.ap[a] = state.ap[a] - 5
-        state.hp[t] = state.hp[t] - 5
-        return state
-    else:
-        return False
+    state.ap[a] = state.ap[a] - act_ap['shoot']
+    state.hp[t] = state.hp[t] - dmg['shoot']
+    return state
 
 
-declare_operators(shoot)
+def throw_grenade(state, a, t):
+    state.ap[a] = state.ap[a] - act_ap['throw_grenade']
+    state.hp[t] = state.hp[t] - dmg['throw_grenade']
+    state.weapons[a].remove('grenade')
+    return state
+
+
+declare_operators(shoot, throw_grenade)
 
 
 def is_done(state, goal):
@@ -21,20 +27,29 @@ def is_done(state, goal):
     return False
 
 
-def attack(state, a, t, goal):
+def assault(state, a, t, assault_type, goal):
     if is_done(state, goal):
         return []
-    while any(state.ap[a] >= x for x in list(move_ap.values())):
-        return [('shoot', a, t), ('act', a, t, goal)]
+    while state.ap[a] >= act_ap[assault_type] \
+            and assault_weapon[assault_type] in state.weapons[a]:
+        return [(assault_type, a, t), ('act', a, t, goal)]
     return False
 
 
-declare_methods('act', attack)
+def rifle_assault(state, a, t, goal):
+    return assault(state, a, t, 'shoot', goal)
+
+
+def grenade_assault(state, a, t, goal):
+    return assault(state, a, t, 'throw_grenade', goal)
+
+
+declare_methods('act', rifle_assault, grenade_assault)
 
 state1 = State('state1')
-state1.weapons = {'ally': ['rifle'], 'enemy': ['rifle']}
-state1.hp = {'ally': 20, 'enemy': 10}
-state1.ap = {'ally': 20, 'enemy': 10}
+state1.weapons = {'ally': ['rifle', 'grenade'], 'enemy': ['rifle']}
+state1.hp = {'ally': 20, 'enemy': 20}
+state1.ap = {'ally': 15, 'enemy': 10}
 
 goal1 = Goal('goal1')
 goal1.hp = {'enemy': 0}
